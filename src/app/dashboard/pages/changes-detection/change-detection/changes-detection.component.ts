3709e445a3da39dd63cb21aef76a0b216100274e
tuNombre <kty.cont26@gmail.com>
2024-04-17 17:54:49 -0600

    components

---
+++ src/app/dashboard/pages/changes-detection/change-detection/changes-detection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './changes-detection.component.html',
+  styles: ``
+})
+export class ChangesDetectionComponent {
+
+}
