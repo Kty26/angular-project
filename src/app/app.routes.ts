a46369d0a0647f40de093bd35242e809aa4ed154
tuNombre <kty.cont26@gmail.com>
2024-04-23 18:33:26 -0600

    side-menu'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,44 +7,53 @@ export const routes: Routes = [
         loadComponent: () =>import('./dashboard/dashboard.component'),
 
         children:[
-            {
-                path: 'changes-detection',
-                title: 'Changes Detection',
-                loadComponent: () => import('./dashboard/pages/changes-detection/changes-detection.component')
-            },
-            {
-                path:'control-flow',
-                title: 'Control Flow',
-                loadComponent: () => import('./dashboard/pages/control-flow/control-flow.component'),
-            },
-            {
-                path:'defer-views',
-                title: 'Defer View',
-                loadComponent: () => import('./dashboard/pages/defer-views/defer-views.component'),
-            },
-            {
-                path:'defer-option',
-                title: 'Defer Option',
-                loadComponent: ()=> import('./dashboard/pages/defer-option/defer-option.component'),
-            },
-            {
-                path:'user/:id',
-                title: 'User View',
-                loadComponent: () => import('./dashboard/pages/user/user.component'),
-            },
-            {
-                path:'users-list',
-                title: 'Users List',
-                loadComponent: () => import('./dashboard/pages/users/users.component'),
-            },
-            {
-                path:'view-transition',
-                title:'View Transition',
-                loadComponent: ()=> import('./dashboard/pages/view-transition/view-transition.component'),
-            },
-            {
-                path:'', redirectTo: 'control-flow', pathMatch: 'full',
-            }
+           {
+            path: 'change-detection',
+            title: 'Change detection',
+            loadComponent: () =>import('./dashboard/pages/changes-detection/changes-detection.component'),
+           } ,
+
+           {
+            path: 'control-flow',
+            title: 'Control flow',
+            loadComponent: () =>import('./dashboard//pages/control-flow/control-flow.component'),
+           },
+           
+           {
+            path: 'defer-option',
+            title: 'Defer option',
+            loadComponent: () =>import('./dashboard/pages/defer-option/defer-option.component'),
+           },
+
+           {
+            path: 'defer-view',
+            title: 'Defer view',
+            loadComponent: () =>import('./dashboard/pages/defer-views/defer-views.component'),
+           },
+
+           {
+            path: 'user/:id',
+            title: 'User View',
+            loadComponent: () =>import('./dashboard/pages/user/user.component'),
+           },
+
+           {
+            path: 'user-list',
+            title: 'User List',
+            loadComponent: () =>import('./dashboard/pages/users/users.component'),
+           },
+
+           {
+            path: 'view-transition',
+            title: 'View transition',
+            loadComponent: () =>import('./dashboard/pages/view-transition/view-transition.component'),
+           },
+
+           {
+            path:'',
+            redirectTo: 'control-flow',
+            pathMatch: 'full'
+           }
         ]
     },
     {
