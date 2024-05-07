0bbd44e78d252133ad57ecaf9b5ca6871eb4b336
tuNombre <kty.cont26@gmail.com>
2024-05-06 23:48:48 -0600

    control-flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
+type Grade= 'A' | 'B' | 'F';
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
   styles: ``
 })
 export default class ControlFlowComponent {
 
+  public showContent = signal(false);
+  public grade = signal<Grade>('A');
+
+  //metodo para cabiar la señal... en este  caso del boton click me
+ public toggleContent() {
+  this.showContent.update(value => !value);
+ }
 }
